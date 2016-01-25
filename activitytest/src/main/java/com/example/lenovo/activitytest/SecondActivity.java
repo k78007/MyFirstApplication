4a1348c064a28df176ede739f11d9c9e1b430b79
james <2300589815@qq.com>
2016-01-26 00:18:32 +0800

    Initial commit

---
+++ activitytest/src/main/java/com/example/lenovo/activitytest/SecondActivity.java
@@ -0,0 +1,14 @@
+package com.example.lenovo.activitytest;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by lenovo on 2015/12/17.
+ */
+public class SecondActivity extends Activity{
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.second_layout);
+    }
+}
